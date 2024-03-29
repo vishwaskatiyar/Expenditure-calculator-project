14c2641aebfc981b5b89eddc30f57f9d8d9e8609
Vishwas katiyar <vishwaskatiyar39@gmail.com>
2024-03-30 00:29:26 +0530

    log in page created

---
+++ script/index.js
@@ -0,0 +1,50 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+    container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+    container.classList.remove("sign-up-mode");
+});
+
+// For Signup Form
+document.getElementById("createAccountForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    var email = document.getElementById("signupEmail").value;
+    var password = document.getElementById("signupPassword").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+
+    if (password !== confirmPassword) {
+        alert("Passwords do not match. Please try again.");
+        return;
+    }
+
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+
+    alert("Account created successfully!");
+    window.location.href = "index.html";
+});
+
+// For Login Form
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    var email = document.getElementById("loginEmail").value;
+    var password = document.getElementById("loginPassword").value;
+    var storedEmail = localStorage.getItem("email");
+    var storedPassword = localStorage.getItem("password");
+
+    if (email === storedEmail && password === storedPassword) {
+        alert("Login successful!");
+        // Redirect or perform any other action after successful login
+        window.location.href = "dashboard.html";
+    } else if (email === storedEmail && password !== storedPassword) {
+        alert("Incorrect password. Please try again.");
+    } else if (email !== storedEmail) {
+        alert("Email not found. Creating a new account.");
+        window.location.href = "index.html";
+    }
+});
